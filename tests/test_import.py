083c9e5de11cfc31e04516302f99f6d334b576ac
Mark <mark.hall@work.room3b.eu>
2017-12-15 12:34:49 +0000

    Merged all import tests into one file

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,3 +1,32 @@
+def test_load_basic_schema(author_schema, author_jsonapi):
+    '''Tests that loading a basic schema with no relationships works'''
+    author, errors = author_schema().load(author_jsonapi)
+    assert errors == {}
+    assert author['id'] == int(author_jsonapi['data']['id'])
+    assert author['name'] == author_jsonapi['data']['attributes']['name']
+
+
+def test_load_basic_missing_field(author_schema, author_jsonapi):
+    '''Tests that loading missing data leads to an error'''
+    del author_jsonapi['data']['attributes']['name']
+    author, errors = author_schema().load(author_jsonapi)
+    assert errors == {'name': ['Missing data for required field.']}
+
+
+def test_load_multiple(tag_schema, tags_jsonapi):
+    tags, errors = tag_schema().load(tags_jsonapi, many=True)
+    assert errors == {}
+    assert len(tags) == 3
+    for tag in tags:
+        assert 'tag' in tag
+
+
+def test_load_multiple_set_on_schema(tag_schema, tags_jsonapi):
+    tags, errors = tag_schema(many=True).load(tags_jsonapi)
+    assert errors == {}
+    assert len(tags) == 3
+    for tag in tags:
+        assert 'tag' in tag
 def test_load_single_parent_child_relationship(author_schema, tag_schema, author_with_interests_jsonapi):
     '''Tests that loading a basic schema with a one-to-many relationships works'''
     author, errors = author_schema(include_schemas=(tag_schema,)).load(author_with_interests_jsonapi)
